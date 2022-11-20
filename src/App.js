3e84aff61dbfebbdce7610993735b1482d416469
Adrian <adrian.gorniak97@op.pl>
2022-11-20 16:07:28 +0100

    axios library installed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [coins, setCoins] = useState([]);
+
   return <div className="App">Test</div>;
 }
 
