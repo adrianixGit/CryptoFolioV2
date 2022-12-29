552a50e3a4d1a20d7e2df2f898ff04f8135bd1f9
Adrian <adrian.gorniak97@op.pl>
2022-12-29 20:03:44 +0100

    fix bugs with .env file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_USERS_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  authDomain: process.env.AUTH_DOMAIN_USERS,
+  projectId: "users-cryptofolio",
+  storageBucket: process.env.STORAGE_BUCKET_USERS,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID_USERS,
+  appId: process.env.APP_ID_USERS,
+  measurementId: process.env.MEASUREMENT_ID_USERS,
+  // authDomain: process.env.AUTH_DOMAIN_USERS,
+  // projectId: process.env.PROJECT_ID_USERS,
+  // storageBucket: process.env.STORAGE_BUCKET_USERS,
+  // messagingSenderId: process.env.MESSAGING_SENDER_ID_USERS,
+  // appId: process.env.APP_ID_USERS,
+  // measurementId: process.env.MEASUREMENT_ID_USERS,
 };
 
 const app = initializeApp(firebaseConfig);
